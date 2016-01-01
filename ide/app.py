bef47804c2fb5233837fb51aa7199e7abb54c72b
skymoneyc <CChain0615@gmail.com>
2016-01-01 11:24:08 +0800

    update app

--- ide/app.py
+++ ide/app.py
@@ -2,7 +2,7 @@
 
 #author: cheng
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask import render_template
 
 app = Flask(__name__)
@@ -16,6 +16,16 @@ def index():
 #@app.route('/code/complete/', methods=['POST'])
 def code_complete():
 	pass
+
+@app.route('/code/submit/', methods=['POST'])
+def code_submit():
+	if request.method == 'POST':
+		code = request.form['code']
+
+		#提交代码，保存代码并运行
+		#保存代码文件，调用执行环境运行
+
+		return jsonify({'status': 'ok'})
 
 
 if __name__ == '__main__':
