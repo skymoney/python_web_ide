655432fe786d03f0b008c2d970849636b0178960
skymoneyc <CChain0615@gmail.com>
2016-01-08 23:00:31 +0800

    update account

--- idev2/account/models.py
+++ idev2/account/models.py
@@ -2,5 +2,33 @@
 
 from django.db import models
 
+
 class Account(models.Model):
     id = models.AutoField(primary_key=True)
+
+    email = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    passwd = models.CharField(max_length=50)
+
+    active_code = models.CharField(max_length=100)
+    is_active = models.IntegerField()
+
+    class Meta:
+        db_table = 'account'
+
+
+class RuntimeMachine(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    account = models.ForeignKey('account.Account', db_column='account')
+
+    container_id = models.CharField(max_length=50)
+
+    mem_limit = models.IntegerField()
+    cpu_limit = models.IntegerField()
+    volume_dir = models.CharField(max_length=500)
+
+    last_modify = models.DateTimeField()
+
+    class Meta:
+        db_table = 'runtime_machine'
---
+++ idev2/account/views.py
@@ -0,0 +1,7 @@
+#-*- coding:utf-8 -*-
+
+
+def login(request):
+    if request.method == 'POST':
+        pass
+
